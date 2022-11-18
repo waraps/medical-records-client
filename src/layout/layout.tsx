3cf6c3415cd87b3a6d4fe359a1b20567e65a3c86
Manuel Meneses <manuelmenesesg@gmail.com>
2022-11-18 13:04:59 +0000

    chore: errors boundaries handled

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 import { ILeftBar, LeftBar, Navbar } from '../components';
+import ErrorBoundary from '../ErrorBoundary';
 import { IAppState } from '../interfaces';
 import { UsersActions } from '../redux/users';
 import { IUsersReducer } from '../redux/users/IUsersReducer';
@@ -25,7 +26,9 @@ const Layout = ({ links }: ILeftBar): JSX.Element => {
             <section className="flex">
                 <LeftBar links={links} />
                 <div className="p-4 w-screen">
-                    <Outlet />
+                    <ErrorBoundary>
+                        <Outlet />
+                    </ErrorBoundary>
                 </div>
             </section>
         </main>
