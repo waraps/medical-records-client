871cec292ff40203d6f9ed185e04b5f14c0b53f9
Manuel Meneses <manuelmenesesg@gmail.com>
2023-01-02 21:06:46 +0000

    fix: misspelling

--- src/pages/auth/signin/signin-schema.tsx
+++ src/pages/auth/signin/signin-schema.tsx
@@ -2,7 +2,7 @@ import * as yup from 'yup';
 
 export const signinSchema = yup.object({
     email: yup.string().label('email').email('Debe ingresar un correo valido').required('Este campo es requerido'),
-    password: yup.string().label('password').min(8, 'Debe Ingresar una contrasena mas larga').required('Este campo es requerido')
+    password: yup.string().label('password').min(8, 'La longitud de la contraseña al menos debe ser de 8 caracteres').required('Este campo es requerido')
 });
 
 export type signinSchemaType = yup.InferType<typeof signinSchema>
