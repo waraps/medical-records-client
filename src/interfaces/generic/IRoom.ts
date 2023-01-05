c036bb30ab85e8a38714b4be48c05b2c4f4c6c6e
Manuel Meneses <manuelmenesesg@gmail.com>
2023-01-05 16:29:02 +0000

    chore: IRoom interface added

---
+++ src/interfaces/generic/IRoom.ts
@@ -0,0 +1,10 @@
+export interface IRoom {
+    id: number;
+    status: string;
+    patient_id: number;
+    patient: string;
+    doctor_id: number;
+    doctor: string;
+    createdAt: string;
+    updatedAt: string;
+}
--- src/interfaces/generic/index.ts
+++ src/interfaces/generic/index.ts
@@ -1,2 +1,3 @@
 export * from './IError';
 export * from './ILinkItem';
+export * from './IRoom';
